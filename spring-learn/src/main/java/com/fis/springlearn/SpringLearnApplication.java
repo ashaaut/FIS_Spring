c05419b0540c57a4ca2bd05180fd43fdf2f8f977
ashaaut <ashaautade1998@gmail.com>
2021-08-09 17:29:30 +0530

    created simple-date format

--- spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
@@ -1,13 +1,30 @@
 package com.fis.springlearn;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
+		displayDate();
+	}
+	static void displayDate()  {
+		 ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		 SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		 try {
+			Date parseDate = (Date) format.parse("31/12/2018");
+			System.out.println(parseDate);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
