4c22ea35cd28a287b0bfc757784cb761a133d63d
ashaaut <ashaautade1998@gmail.com>
2021-08-11 17:39:26 +0530

    added employee JPA

---
+++ spring-jpa/src/main/java/com/fis/springjpa/employee/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.fis.springjpa.employee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/employee")
+public class EmployeeController {
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	 @PostMapping(path="/add")
+	 public @ResponseBody String addNewEmp(@RequestParam int id
+		      , @RequestParam String name) {
+		 
+		 Employee employee=new Employee();
+		 employee.setId(id);
+		 employee.setName(name);
+		 employeeRepository.save(employee);
+		 return "saved";
+	 }
+	@GetMapping(path="/all")
+	public @ResponseBody Iterable<Employee>getAllEmployee(){
+		return employeeRepository.findAll();
+		
+	}
+}
---
+++ spring-jpa/src/main/java/com/fis/springjpa/employee/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.fis.springjpa.employee;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
+}
